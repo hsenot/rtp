c546c4de3ba93cb4633937da7ab67c635e0b98fb
Herve Senot <hsenot@gmail.com>
2018-05-20 23:13:04 +1000

    Logging, to see things

--- rtp/rtp/settings/dev.py
+++ rtp/rtp/settings/dev.py
@@ -22,3 +22,46 @@ DATABASES = {
         'PORT': get_secret("DEFAULT_DB_PORT", secrets),
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'level': 'DEBUG',
+        'handlers': [
+            'console',
+        ],
+    },
+    'formatters': {
+        'verbose': {
+            'format': "%(name)s %(filename)s line %(lineno)d [%(asctime)s] %(levelname)s %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'urllib3': {
+            'handlers': [],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'requests': {
+            'handlers': [],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
+
+APIS = get_secret("APIS", secrets)
