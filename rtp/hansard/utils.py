05c55800fa9e200e7747823fc26648e449940c42
Herve Senot <hsenot@gmail.com>
2018-06-09 22:51:04 +1000

    Better parsing of hansard full text, hence better NLP results :-)

--- rtp/hansard/utils.py
+++ rtp/hansard/utils.py
@@ -4,7 +4,7 @@ import logging
 import spacy
 
 from lxml import etree
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 from textblob import TextBlob
 
 from nltk.corpus import stopwords
@@ -15,130 +15,62 @@ from nltk.chunk import tree2conlltags
 
 logger = logging.getLogger(__name__)
 
+
+# Contextualise a tag within a hansard file 
+# by extracting speaker, type, time and cleaned text
+def contextualise_tag(tag):
+    try:
+        speech = {}
+        speech_header = tag.parent.p.span
+        # returns the full context for a sentence
+        # Type classes
+        # TODO: HPS-MemberContinuation?
+        type_classes = ['HPS-MemberSpeech', 'HPS-MemberQuestion', 'HPS-MemberAnswer', 'HPS-OfficeSpeech']
+        speech['type'] = speech_header.find(attrs={'class':type_classes})['class']
+        speech['person'] = speech_header.find(attrs={'class':type_classes}).get_text()
+        speech['time'] = speech_header.find(attrs={'class':'HPS-Time'}).get_text()
+
+        # First element: a bit of wrangling to get the useful text
+        if tag==tag.parent.p:
+            siblings = [si if isinstance(si, element.NavigableString) else si.get_text() for si in tag.find(attrs={'class':'HPS-Time'}).next_siblings]
+            speech['text'] = "".join(siblings)[4:]
+        else:
+            # Other elements are more straight forward
+            speech['text'] = tag.get_text().strip('\n')
+
+        return speech
+
+    except Exception as e:
+        logger.debug("Error contextualising tag: %s" % (tag,))
+
+
+# Returns a structured log of actual speeches devoid of procedural ornements, and annotated by their speaker, start time and type
 def parse_hansard(filename='House of Representatives_2018_05_10_6091.xml'):
-    # Returns a structured log of actual speeches devoid of procedural ornements 
-    # Actual speeches are flagged by:
-    # 
-
-    # HPS-MemberSpeech
-    # Note: usually decorated with some regularly expressed markup
-    """
-                    <a href="00APG" type="MemberSpeech">
-                        <span class="HPS-MemberSpeech">The SPEAKER</span>
-                    </a> (<span class="HPS-Time">09:31</span>):
-    """
-    # or
-    """
-                    <a href="HWP" type="MemberSpeech">
-                        <span class="HPS-MemberSpeech">Ms MARINO</span>
-                    </a> (<span class="HPS-Electorate">Forrest</span>—<span class="HPS-MinisterialTitles">Chief Government Whip</span>) (<span class="HPS-Time">09:31</span>):
-    """
-
-    # HPS-MemberQuestion
-    """
-                    <a href="M3C" type="MemberQuestion">
-                        <span class="HPS-MemberQuestion">Mr BANDT</span>
-                    </a> (<span class="HPS-Electorate">Melbourne</span>) (<span class="HPS-Time">14:19</span>):
-    """
-    # or
-    """
-                    <a href="00ATG" type="MemberQuestion">
-                        <span class="HPS-MemberQuestion">Mr SHORTEN</span>
-                    </a> (<span class="HPS-Electorate">Maribyrnong</span>—<span class="HPS-MinisterialTitles">Leader of the Opposition</span>) (<span class="HPS-Time">14:25</span>):
-    """
-    # HPS-MemberAnswer
-    """
-                    <a href="208884" type="MemberAnswer">
-                        <span class="HPS-MemberAnswer">Mr PORTER</span>
-                    </a> (<span class="HPS-Electorate">Pearce</span>—<span class="HPS-MinisterialTitles">Attorney-General</span>) (<span class="HPS-Time">14:27</span>):
-    """
-    # HPS-MemberContinuation
-    """
-                    <a href="E3L" type="MemberContinuation">
-                        <span class="HPS-MemberContinuation">Mr MORRISON:</span>
-                    </a>
-    """
-    # HPS-MemberInterjecting
-    # Note: HPS-MemberIInterjecting is excluded as it contains no spoken words
-    # Note: HPS-OfficeInterjecting is excluded as it contains only procedural gibberish
-    # Note: HPS-GeneralInterjecting is excluded as it not attributable to anyone
-    # Target structure to be a list of dict
-    # each dict contains:
-    # speaker
-    # type of contribution (speech, question, answer, continuation, interjection)
-    # time (for speech, question and answer)
-    # a list of paragraphs, in the same order they were delivered
     # Supported parsers
     # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
     with open(os.path.join('hansard/data/raw', filename), 'r') as xml: 
         soup = BeautifulSoup(xml.read(), "xml")
 
-    # Cleaning up
-    sample = ''
-    current_speaker = None
-    fragments, fragment = [], {}
+    # Fragment contextualisation & cleaning
+    fragments = []
+    interjection_classes = ['HPS-MemberInterjecting', 'HPS-MemberIInterjecting', 'HPS-OfficeInterjecting']
 
-    for talk in soup.find_all('talk.text')[:3]:
+    for talk in soup.find_all('talk.text'):
         for p in talk.find_all('p'):
-            try:
-                spans = p.find_all('span')
-                for span in spans:
-                    if span.has_attr('class'):
-                        if span['class'] in ["HPS-MemberSpeech", "HPS-MemberQuestion", "HPS-MemberAnswer"]:
-                            # Changing speaker: need a new fragment
-                            print("Fragment: %s" % (fragment))
-                            if fragment.keys():
-                                fragments.append(fragment)
-                            fragment = {
-                                'speaker': span.get_text(),
-                                'type': span['class'],
-                                'time': None,
-                                'text': []
-                            }
-                        elif span['class'] in ["HPS-Time"]:
-                            fragment['time'] = span.get_text()
-                        elif span['class'] in ["HPS-Normal"]:
-                            if 'text' in fragment.keys():
-                                fragment['text'] += [span.get_text()]
-                            else:
-                                fragment['text'] = [span.get_text()]
-                        else:
-                            # Unrecognised class
-                            pass
-                    else:
-                        # Non-tagged spans not interesting
-                        pass
+            if p.find(attrs={'class': interjection_classes}):
+                pass
+            else:
+                fragments.append(contextualise_tag(p))
 
-            except AttributeError as e:
-                # Not a tag element, probably a string like '\n'
-                print(str(e))
+    sample = " ".join([frag['text'] for frag in fragments])
+    return soup, sample
 
 
-
-    # Add a space after each dot
-    #raw_text = talk.get_text(strip=True).replace('.', '. ')
-    #sample += raw_text
-
-
-
-def process_hansard_file(filename='House of Representatives_2018_05_10_6091.xml'):
-    # Supported parsers
-    # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
-    with open(os.path.join('hansard/data/raw', filename), 'r') as xml: 
-        soup = BeautifulSoup(xml.read(), "xml")
-
+def analyse_hansard_file(filename='House of Representatives_2018_05_10_6091.xml'):
     # Word frequency analysis
     my_abbrev = ['\'m', '.', ',', '\'s', '(', ')', 'n\'t', '\'ve', ';', '$', ':', '\'', '?', '\'ll', '\'re']
     stoplist = set(stopwords.words('english') + my_abbrev)
-    sample = ''
-    for talk in soup.find_all('talk.text'):
-        # TODO: clean up the junk
-        # TODO: remove "(<span class="HPS-Time">09:31</span>):"
-        # TODO: remove "(<span class="HPS-Electorate">Forrest</span>—<span class="HPS-MinisterialTitles">Chief Government Whip</span>):"
-        # TODO: other embedded elements not part of the talk?
-        # Add a space after each dot
-        raw_text = talk.get_text(strip=True).replace('.', '. ')
-        sample += raw_text
+    soup, sample = parse_hansard(filename)
 
     # Tokenisation, tagging, chunking
     sent_tokenizer = PunktSentenceTokenizer()
@@ -163,33 +95,6 @@ def process_hansard_file(filename='House of Representatives_2018_05_10_6091.xml'
     for sentence in tagged_sentences:
         tags += sentence
     pos_analysis(tags, my_abbrev)
-
-    # Same with TextBlob -> produces the exact same output
-    #sample_blob = TextBlob(sample)
-    #pos_analysis(sample_blob.tags, my_abbrev)
-
-    # Get named entities
-    # nltk way -> label extraction for a continuous chunk needs improvement
-    named_entities_tags = []
-    for sentence in chunked_sentences:
-        named_entities_tags += get_continuous_chunks(sentence)
-    display_freq([ne for ne, ne_type in named_entities_tags if ne_type=='PERSON'], 'Named entities (PERSON)', top=20)
-
-    # Stanford NER tagger
-    jar = 'hansard/stanford-ner-tagger/stanford-ner.jar'
-    model = 'hansard/stanford-ner-tagger/english.all.3class.distsim.crf.ser.gz'
-
-    try:
-        # Prepare NER tagger with english model
-        ner_tagger = StanfordNERTagger(model, jar, encoding='utf8')
-        named_entities_stanford = []
-        # TODO: far too slow - how can this be useful at all?
-        #for sentence in tokenized_sentences:
-        #    named_entities_stanford += ner_tagger.tag(sentence)
-        #display_freq([ne for ne, ne_type in named_entities_stanford if ne_type!='O'], 'Named entities (Stanford)', top=100)
-    except LookupError as e:
-        logger.error("Are the Stanford NER tagger JAR and model in the stanford-ner-tagger folder?")
-        logger.error("Hint: https://blog.sicara.com/train-ner-model-with-nltk-stanford-tagger-english-french-german-6d90573a9486")
 
     # spaCy NER
     nlp = spacy.load('en_core_web_sm')
@@ -230,6 +135,3 @@ def pos_analysis(tags, stoplist):
     display_freq(adjectives, 'Adjectives', top=50)
     verbs = [wordnet_lemmatizer.lemmatize(word, pos='v') for word, tag in tags if tag[:2] in ('VB') and word not in stoplist]
     display_freq(verbs, 'Verbs', top=50)
-
-def get_continuous_chunks(sentence):
-    return [(" ".join(w for w, t in elt), [t for w, t in elt][-1]) for elt in sentence if isinstance(elt, nltk.tree.Tree)]
