bc7be47173b357a6a7212d69fb7085c13049ca65
Herve Senot <hsenot@gmail.com>
2018-06-17 12:56:35 +1000

    Fixed renamed electorate model

--- rtp/theyvoteforyou/utils.py
+++ rtp/theyvoteforyou/utils.py
@@ -4,7 +4,7 @@ import requests
 
 from django.conf import settings
 
-from aec.models import federal_electorate_2016
+from aec.models import FederalElectorate2016
 
 logger = logging.getLogger(__name__)
 api = settings.APIS['THEYVOTEFORYOU']
@@ -27,7 +27,7 @@ def check_electorates():
     # TheyWorkForYou
     tv4u_electorates = [peep['latest_member']['electorate'] for peep in api_call('people') if peep['latest_member']['house']=='representatives']
     # AEC electorates
-    aec_electorates = list(federal_electorate_2016.objects.all().values_list('elect_div', flat=True))
+    aec_electorates = list(FederalElectorate2016.objects.all().values_list('elect_div', flat=True))
 
     unknown_to_aec = set(tv4u_electorates) - set(aec_electorates)
     logger.debug("Unknown to AEC: %s" % unknown_to_aec)
